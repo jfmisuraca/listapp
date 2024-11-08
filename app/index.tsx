faee4c914da8aa314ab79ca2c67e6301cc7a6e25
Juan Francisco <jfmisuraca@hotmail.com>
2024-11-08 06:48:33 -0300

    jstodotxt testing

--- app/index.tsx
+++ app/index.tsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import * as SplashScreen from 'expo-splash-screen';
 import { material } from "react-native-typography";
 import styles from '../components/styles';
+import { Item } from "jstodotxt";
 
 SplashScreen.preventAutoHideAsync();
 
@@ -14,11 +15,12 @@ export default function Index() {
     }
     prepare();
   }, []);
+  const item = new Item('este es un item de prueba @home')
   return (
     <View
       style={styles.container}>
       <Text style={material.headline}>Soy el index (listas)</Text>
-      <Button title="Botón" onPress={() => alert('auch')} />
+      <Button title="clg" onPress={() => console.log(item.contexts())} />
     </View>
   );
 }
