b58448a0691fa365a5658a9d4ea7c34b1dfdc0c3
Juan Francisco <jfmisuraca@hotmail.com>
2024-11-11 19:29:47 -0300

    agrega lógica para guardar entrada en filesystem

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View, Button } from "react-native";
-import React, { useEffect } from "react";
+import { Text, View, Button, TextInput } from "react-native";
+import React, { useEffect, useState } from "react";
 import * as SplashScreen from 'expo-splash-screen';
 import { material } from "react-native-typography";
 import styles from '../components/styles';
 import { Item } from "jstodotxt";
+import * as FileSystem from 'expo-file-system';
 
 SplashScreen.preventAutoHideAsync();
 
@@ -15,12 +16,62 @@ export default function Index() {
     }
     prepare();
   }, []);
-  const item = new Item('este es un item de prueba @home')
+  const [inputText, setInputText] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  const fileUri = `${FileSystem.documentDirectory}data.json`;
+
+  const handleAddText = async () => {
+    if (inputText.trim() === '') return;
+    const newMessages = [...messages, inputText];
+
+    try {
+      await FileSystem.writeAsStringAsync(fileUri, JSON.stringify(newMessages));
+      setMessages(newMessages);
+      setInputText('');
+    } catch (error) {
+      console.error('error al escribir en el archivo', error);
+    }
+  };
+  const loadMessages = async () => {
+    try {
+      const fileContent = await FileSystem.readAsStringAsync(fileUri);
+      setMessages(JSON.parse(fileContent));
+    } catch (error) {
+      console.error('Error al leer el archivo:', error);
+    }
+  };
+
+  // Carga los mensajes cuando se monta el componente
+  React.useEffect(() => {
+    loadMessages();
+  }, []);
+
   return (
-    <View
-      style={styles.container}>
-      <Text style={material.headline}>Soy el index (listas)</Text>
-      <Button title="clg" onPress={() => console.log(item.contexts())} />
+    <View style={{ padding: 20 }}>
+      <TextInput
+        placeholder="Escribe algo..."
+        value={inputText}
+        onChangeText={setInputText}
+        style={{ borderWidth: 1, padding: 10, marginBottom: 10 }}
+      />
+      <Button title="Agregar" onPress={handleAddText} />
+      <Text style={{ marginTop: 20, fontWeight: 'bold' }}>Mensajes:</Text>
+      {messages.map((message, index) => (
+        <Text key={index}>{message}</Text>
+      ))}
     </View>
   );
-}
+};
+
+
+/* const item = new Item('este es un item de prueba @home')
+ return (
+   <View
+     style={styles.container}>
+     <Text style={material.headline}>Soy el index (listas)</Text>
+     <TextInput></TextInput>
+     <Button title="clg" onPress={() => console.log(item.contexts())} />
+   </View>
+ );
+} */
